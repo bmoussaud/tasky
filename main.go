5530795904fda6d21005f09c27379df036f0d64f
Benoit Moussaud <benoit@moussaud.org>
2024-06-19 06:34:54 +0200

    fix k8S

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func index(c *gin.Context) {
 func main() {
 	godotenv.Overload()
 
-	gin.SetMode(gin.DebugMode)
+	//gin.SetMode(gin.DebugMode)
 
 	router := gin.Default()
 	router.LoadHTMLGlob("assets/*.html")
